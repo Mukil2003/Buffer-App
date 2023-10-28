b6072795ed004a4f45c5dd30d80c7ae29b2e959b
JubieSparkle <sparkle15803@gmail.com>
2023-10-28 18:20:59 +0530

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Challenge from "./Components/FirstPage/Challenge";
+import Navbar from "./Components/FirstPage/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Challenge />
+      <Navbar />
     </div>
   );
 }
 
 export default App;
-
-//<i class="fa-brands fa-buffer"></i>
