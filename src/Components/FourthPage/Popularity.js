f22afef83f01d8cda10ba1e60da97e6068a60c57
JubieSparkle <sparkle15803@gmail.com>
2023-11-07 20:13:34 +0530

    Successfully created fourth  page

---
+++ src/Components/FourthPage/Popularity.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Testimonials.css";
+
+function Popularity() {
+  return (
+    <div className="popularity">
+      <div className="popularity-box">
+        <h2>10 years</h2>
+        <p>in business</p>
+      </div>
+
+      <div className="popularity-box">
+        <h2>140,000</h2>
+        <p>users</p>
+      </div>
+
+      <div className="popularity-box">
+        <h2>100k+</h2>
+        <p>monthly blog readers</p>
+      </div>
+
+      <div className="popularity-box">
+        <h2>1.2m+</h2>
+        <p>social followers</p>
+      </div>
+    </div>
+  );
+}
+
+export default Popularity;
---
+++ src/Components/FourthPage/Testimonials.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Testimonials.css";
+import Popularity from "./Popularity";
+
+function Testimonials() {
+  return (
+    <div className="testimonials">
+      <Popularity />
+    </div>
+  );
+}
+
+export default Testimonials;
