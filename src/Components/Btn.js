d54403ffffd6b22f013378c5e4b12cd1493831ba
JubieSparkle <sparkle15803@gmail.com>
2023-11-07 20:31:03 +0530

    Updated button

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -6,11 +6,11 @@ function Btn({ contents }) {
     color: `${contents.color}`,
     padding: `${contents.padding}`,
     fontFamily: "Roboto",
-    border: "none",
     outline: "none",
     borderRadius: "5px",
     fontSize: `${contents.fontSize}`,
     cursor: "pointer",
+    border: `${contents.border}`,
   };
 
   return (
